2f8b2940f55580362af948842ddeb8d1a821e8bd
leesemin <leesemin222@gmail.com>
2023-04-27 12:54:11 +0900

    파일 삭제

--- commander-1.js
+++
@@ -1,8 +0,0 @@
-import { program } from "commander";
-
-const app = program.command('kdt')
-app.action(()=>{
-  console.log('wow! 내가 만든 명령어!')
-})
-
-program.parse(process.argv)
--- commander-2.js
+++
@@ -1,9 +0,0 @@
-import { program } from "commander";
-
-const app = program.command(`add <a> <b>`)
-app.action((a, b)=>{
-  const result = Number(a) + Number(b)
-  console.log(`${a}+${b} = ${result}`)
-})
-
-program.parse(process.argv)
